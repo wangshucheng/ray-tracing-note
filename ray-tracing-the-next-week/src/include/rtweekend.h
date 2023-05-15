f41481a4779175bb31bc70b63d22f93a13aee5b8
ShushingWong <122181582@qq.com>
2023-05-15 22:33:47 +0800

    init

---
+++ ray-tracing-the-next-week/src/include/rtweekend.h
@@ -0,0 +1,67 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+
+// Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * pi / 180;
+}
+
+inline double ffmin(double a, double b) { return a <= b ? a : b; }
+inline double ffmax(double a, double b) { return a >= b ? a : b; }
+
+inline double random_double()
+{
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+// #include <functional>
+// #include <random>
+
+// inline double random_double()
+// {
+//     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//     static std::mt19937 generator;
+//     static std::function<double()> rand_generator =
+//         std::bind(distribution, generator);
+//     return rand_generator();
+// }
+
+inline double clamp(double x, double min, double max)
+{
+    if (x < min)
+        return min;
+    if (x > max)
+        return max;
+    return x;
+}
+
+// Common Headers
+
+// #include "ray.h"
+
+#endif
