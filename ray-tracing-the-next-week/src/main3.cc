8135b0b4cdeff945facfe298f400fe80557171ec
ShushingWong <122181582@qq.com>
2023-05-16 02:03:16 +0800

    Bounding Volume Hierarchies

---
+++ ray-tracing-the-next-week/src/main3.cc
@@ -0,0 +1,150 @@
+#include "rtweekend.h"
+
+#include "hittable_list.h"
+#include "sphere.h"
+#include "moving_sphere.h"
+#include "camera.h"
+#include "material.h"
+#include "bvh.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include <iostream>
+
+vec3 ray_color(const ray &r, const hittable &world, int depth)
+{
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return vec3(0, 0, 0);
+
+    if (world.hit(r, 0.001, infinity, rec))
+    {
+        ray scattered;
+        vec3 attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth - 1);
+        return vec3(0, 0, 0);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+hittable_list random_scene()
+{
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9)
+            {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8)
+                {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    auto center2 = center + vec3(0, random_double(0, .5), 0);
+                    world.add(make_shared<moving_sphere>(
+                        center, center2, 0.0, 1.0, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else
+                {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
+int main()
+{
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 256;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 50;
+    const int max_depth = 50;
+
+    // std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    double time0 = 0.0;
+    double time1 = 1.0;
+
+    // World
+
+    auto world = random_scene();
+    auto world_bvh_node = bvh_node(world, time0, time1);
+
+    // Camera
+
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, time0, time1);
+
+    unsigned char *data = new unsigned char[image_width * image_height * 3];
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i)
+        {
+            vec3 color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / image_width;
+                auto v = (j + random_double()) / image_height;
+                ray r = cam.get_ray(u, v);
+                color += ray_color(r, world_bvh_node, max_depth);
+            }
+            // color.write_color(std::cout, samples_per_pixel);
+
+            vec3 col = color.write_color(samples_per_pixel);
+            data[(image_height - j - 1) * image_width * 3 + i * 3] = (unsigned char)(255.99f * col[0]);
+            data[(image_height - j - 1) * image_width * 3 + i * 3 + 1] = (unsigned char)(255.99f * col[1]);
+            data[(image_height - j - 1) * image_width * 3 + i * 3 + 2] = (unsigned char)(255.99f * col[2]);
+        }
+    }
+
+    stbi_write_jpg("..//output.jpg", image_width, image_height, 3, data, 100);
+    delete[] data;
+
+    std::cerr << "\nDone.\n";
+}
