305ce5985c9c20002be8b3c73c0e89b20bc27c87
ShushingWong <122181582@qq.com>
2023-05-17 22:41:05 +0800

    texture

--- ray-tracing-the-next-week/include/material.h
+++ ray-tracing-the-next-week/include/material.h
@@ -3,6 +3,7 @@
 #define MATERIAL_H
 
 #include "hittable.h"
+#include "texture.h"
 
 class material
 {
@@ -14,19 +15,20 @@ public:
 class lambertian : public material
 {
 public:
-    lambertian(const vec3 &a) : albedo(a) {}
+    lambertian(const color &a) : albedo(make_shared<solid_color>(a)) {}
+    lambertian(shared_ptr<texture> a) : albedo(a) {}
 
     virtual bool scatter(
         const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const
     {
         vec3 scatter_direction = rec.normal + random_unit_vector();
         scattered = ray(rec.p, scatter_direction, r_in.time());
-        attenuation = albedo;
+        attenuation = albedo->value(rec.u, rec.v, rec.p);
         return true;
     }
 
 public:
-    vec3 albedo;
+    shared_ptr<texture> albedo;
 };
 
 class metal : public material
