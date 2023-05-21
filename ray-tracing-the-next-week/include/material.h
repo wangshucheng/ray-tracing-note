21e835ac5ad1c48c4cb878bda9b1110f28addf70
ShushingWong <122181582@qq.com>
2023-05-21 22:43:57 +0800

    Rectangles and Lights

--- ray-tracing-the-next-week/include/material.h
+++ ray-tracing-the-next-week/include/material.h
@@ -8,6 +8,11 @@
 class material
 {
 public:
+    virtual color emitted(double u, double v, const point3 &p) const
+    {
+        return color(0, 0, 0);
+    }
+
     virtual bool scatter(
         const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const = 0;
 };
@@ -88,6 +93,27 @@ private:
         r0 = r0 * r0;
         return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
+};
+
+class diffuse_light : public material
+{
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    virtual bool scatter(
+        const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override
+    {
+        return false;
+    }
+
+    virtual color emitted(double u, double v, const point3 &p) const override
+    {
+        return emit->value(u, v, p);
+    }
+
+public:
+    shared_ptr<texture> emit;
 };
 
 #endif
